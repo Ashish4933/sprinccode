438690bf369c6cb954be9c0594d7096168382f55
Ashish Tripathi <ashishtripathi4933@gmail.com>
2024-09-03 23:32:52 +0530

    Inital 2 commit

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,22 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    
+	public String home() {
+		System.out.println("This is home url");
+		return "index";
+	}
+    @RequestMapping("/about")
+    
+    
+    public String about() {
+    	System.out.println("thiis is about url");
+    	return "about";
+    }
+    
+}
